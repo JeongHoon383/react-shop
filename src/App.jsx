d01294d172a0952984c825d8f4df005077c8206f
Jeong Hoon <gbg9609@naver.com>
2024-07-22 21:02:35 +0900

    api 호출, redux에 담아야됨

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import ProductDetail from "./Pages/ProductDetail";
 import Nav from "./Components/Nav";
+import { useEffect, useState } from "react";
 // home 페이지, 로그인 페이지, 제품 상세 페이지
 // 유저는 메뉴와 상품들을 볼 수 있다.
 // 유저는 로그인을 할 수 있다.
@@ -12,9 +13,29 @@ import Nav from "./Components/Nav";
 // 유저는 상품을 검색할 수 있다.
 // 유저는 로그아웃을 할 수 있다.
 
-// navbar 작업 -> 데이터 세팅 Json Server
+// navbar 작업 - o
+// Json 데이터 연결
+// product에 담은 데이터를 redux에 담아서 사용해야함.
 
 function App() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:4000/products";
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((err) => {
+        console.error("에러발생 :", err);
+      });
+  }, []);
+
+  console.log(product);
+
   return (
     <>
       <Nav />
---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+let initialState = [
+  {
+    id: 0,
+    img: "https://noona-hnm.netlify.app/pattern-jacket.jpeg",
+    title: "벨티드 트윌 코트",
+    price: 99900,
+    choice: true,
+    new: true,
+    size: ["S", "M", "L"],
+  },
+];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  }
+};
+
+export default reducer;
