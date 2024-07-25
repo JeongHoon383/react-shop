d3f9ef4a669f96bb2c55d0c7b4c5697b97cf3849
Jeong Hoon <gbg9609@naver.com>
2024-07-25 16:09:48 +0900

    login UI, 유효성 검사 작업 중

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
+// 이메일, 패스워드 창 눌렀을때 검정색으로 테두리 변하게 - o
+// 이메일 @이메일.com 형식에 안맞으면 안내문구 나오게
+// 비밀번호 8자 이상 입력 안하면 "8자 이상 입력해 주세요" 문구 나오게
+// 이메일, 패스워드 조건 맞으면 배경 검정, 글씨색 흰색으로 변경되게
+// 로그인 hover시 색깔 변하게 밝게 - 패스트 캠퍼스 참고
+// 아이디 저장하기 체크박스 디자인 - 패스트 캠퍼스 참고
+
 const Login = () => {
+  const emailRegex =
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   const onSubmit = () => {};
 
   return (
     <div className="Login">
-      <h2>Login</h2>
+      <h2>로그인</h2>
       <form onSubmit={onSubmit} className="Login-form">
-        <div className="Login-box">
-          <input type="email" placeholder="Email" className="Login-email" />
-          <input
-            type="password"
-            placeholder="Password"
-            className="Login-password"
-          />
-        </div>
-        <label htmlFor="id_save">
-          <input type="checkbox" />
-          아이디 저장하기
+        <label htmlFor="email" className="Login-title">
+          이메일
         </label>
-        <button type="submit">로그인</button>
+        <input
+          type="email"
+          placeholder="이메일 주소를 입력해 주세요."
+          className="Login-input"
+        />
+        <label htmlFor="password" className="Login-title">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          placeholder="비밀번호를 입력해 주세요."
+          className="Login-input"
+        />
+        <label htmlFor="checkbox" className="id-checkbox">
+          <input type="checkbox" />
+          <span>아이디 저장하기</span>
+        </label>
+        <button type="submit" className="Login-button">
+          로그인
+        </button>
       </form>
-      <div className="box-wrapper">
-        <div className="box">box1</div>
-        <div className="box">box2</div>
-      </div>
     </div>
   );
 };
