2ad911360176b71f1c0b57bd115c9b94b535611e
Jeong Hoon <gbg9609@naver.com>
2024-08-04 22:03:44 +0900

    Feat : ProductDetail 페이지 UI 작업, Modifying : ProductDetail 페이지 새로고침시 데이터 사라지는 오류 해결중, 세션 스토리지 시도 데이터는 저장이 되지만 각 페이지 마다 삭제 및 생성이 매번 진행되어야 하며 코드가 길어져 적합하지 않다고 판단, Context API 로 데이터를 관리해보는 방법 시도해볼 것

--- src/Pages/ProductDetail.jsx
+++ src/Pages/ProductDetail.jsx
@@ -5,30 +5,32 @@ import "./ProductDetail.css";
 const ProductDetail = ({ product }) => {
   const params = useParams();
 
-  const selectedProduct = product.find(
-    (item) => Number(item.id) == Number(params.id)
+  const curProduct = product.find(
+    (item) => Number(item.id) === Number(params.id)
   );
-  console.log(typeof selectedProduct);
 
   // find 함수까지는 에러가 발생하지 않음
   // find 함수에서 꺼낸 객체 안에 키값을 맨 처음 랜더링 했을 때는 괜찮지만 새로고침시 데이터가 날라감
   // 로컬 또는 세션 스토리지를 이용하여 데이터 저장시 데이터가 날라가지 않는지 해봐야됨.
 
+  // find 함수가 문제 why?
+
   return (
     <div className="ProductDetail">
-      <div>
-        <img src={img} alt="" />
+      <div className="ProductDetail-img">
+        <img src={curProduct.img} alt="" />
       </div>
-      <div>
-        <div>{title}</div>
-        <div>₩{price.toLocaleString()}</div>
-        <div>{choice ? "Conscious choice" : ""}</div>
+      <div className="ProductDetail-content">
+        <div>{curProduct.title}</div>
+        <div>₩{curProduct.price.toLocaleString()}</div>
+        <div>{curProduct.choice ? "Conscious choice" : ""}</div>
         <div>
-          <select name="" id="">
-            <option value=""></option>
-            <option value=""></option>
-            <option value=""></option>
-          </select>
+          <button>사이즈 선택</button>
+          <ul>
+            {curProduct.size.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
         </div>
         <div>
           <button>추가</button>
